e00ec64fadaa8cbac166eff341ad4568bb1625a9
Leo <leonid.inbox@gmail.com>
2014-08-26 19:51:46 +0400

    here you go - line numbers

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,9 @@
+import qualified Text.Parsec             as P
 import qualified Text.Parsec.ByteString  as PB
 import qualified Text.Parsec.Combinator  as PC
 import qualified Text.Parsec.Indent      as PI
 import qualified Text.Parsec.Char        as PChar
+import qualified Text.Parsec.Prim        as PP
 import qualified Data.Monoid             as DM
 import qualified Control.Applicative     as CA
 
@@ -25,12 +27,17 @@ aTag = do
   tabsOrWhitespacesOrNewLines
   PChar.char '%'
   tagName <- PC.many1 PChar.alphaNum
+  position <- PP.getPosition
   tabsOrWhitespacesOrNewLines
-  return (Tag tagName)
+  return (Tag (P.sourceLine position) tagName)
 
 tabsOrWhitespacesOrNewLines = do
-  PC.many1 (PChar.char '\n')
+  CA.many (PC.choice [
+      (PChar.char '\n'),
+      (PChar.char ' ')
+      ])
 
 data SoupOfTags = SoupOfTags [Tag]        deriving (Show)
-data Tag        = Tag String              deriving (Show)
+data Tag        = Tag Line String         deriving (Show)
+type Line = Int
 
