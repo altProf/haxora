1186933b48fe16299e5ca904b71be772c49ddba3
Leo <leonid.inbox@gmail.com>
2014-08-26 21:23:08 +0400

    refactor

--- src/Main.hs
+++ src/Main.hs
@@ -5,7 +5,7 @@ import qualified Text.Parsec.Combinator  as PC
 import qualified Text.Parsec.Char        as PChar
 import qualified Text.Parsec.Prim        as PP
 -- import qualified Data.Monoid             as DM
-import qualified Control.Applicative     as CA
+import           Control.Applicative     as CA
 
 -- data MegaHaml = Empty | Node Element [MegaHaml]
 -- type Element = String
@@ -20,26 +20,20 @@ inputHaml = PB.parseFromFile megaParser "input.haml"
 
 megaParser :: PB.Parser SoupOfTags
 megaParser = do
-  tagList <- PC.many1 aTag
+  tagList <- PC.many1 aTag'
   return (SoupOfTags tagList)
 
-aTag :: PB.Parser Tag
-aTag = do
-  _ <- tabsOrWhitespacesOrNewLines
-  _ <- PChar.char '%'
-  tagName <- PC.many1 PChar.alphaNum
-  position <- PP.getPosition
-  _ <- tabsOrWhitespacesOrNewLines
-  return (Tag (P.sourceLine position) tagName)
-
-tabsOrWhitespacesOrNewLines :: PB.Parser [Char]
-tabsOrWhitespacesOrNewLines = do
-  CA.many (PC.choice [
-      (PChar.char '\n'),
-      (PChar.char ' ')
-      ])
+aTag' :: PB.Parser Tag
+aTag' = Tag <$>
+    (PChar.spaces *>
+    PChar.char '%' *> 
+    (P.sourceLine <$> PP.getPosition))
+    <*>
+    (PC.many1 PChar.alphaNum <*
+    PChar.spaces)
 
 data SoupOfTags = SoupOfTags [Tag]        deriving (Show)
-data Tag        = Tag Line String         deriving (Show)
-type Line = Int
+data Tag        = Tag {
+              line    :: Int,
+              tagName :: String }         deriving (Show)
 
