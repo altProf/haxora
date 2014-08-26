024e8abb84ba6f9a16ad5918d30fb262d9008480
Leo <leonid.inbox@gmail.com>
2014-08-26 19:59:40 +0400

    fix warnings for cabal install --ghc-options="-Wall -fforce-recomp -Werror"

--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,21 @@
 import qualified Text.Parsec             as P
 import qualified Text.Parsec.ByteString  as PB
 import qualified Text.Parsec.Combinator  as PC
-import qualified Text.Parsec.Indent      as PI
+-- import qualified Text.Parsec.Indent      as PI
 import qualified Text.Parsec.Char        as PChar
 import qualified Text.Parsec.Prim        as PP
-import qualified Data.Monoid             as DM
+-- import qualified Data.Monoid             as DM
 import qualified Control.Applicative     as CA
 
-data MegaHaml = Empty | Node Element [MegaHaml]
-type Element = String
+-- data MegaHaml = Empty | Node Element [MegaHaml]
+-- type Element = String
 
 main :: IO ()
 main = do
   megaResult <- inputHaml
   print megaResult
 
+inputHaml :: IO (Either P.ParseError SoupOfTags)
 inputHaml = PB.parseFromFile megaParser "input.haml"
 
 megaParser :: PB.Parser SoupOfTags
@@ -24,13 +25,14 @@ megaParser = do
 
 aTag :: PB.Parser Tag
 aTag = do
-  tabsOrWhitespacesOrNewLines
-  PChar.char '%'
+  _ <- tabsOrWhitespacesOrNewLines
+  _ <- PChar.char '%'
   tagName <- PC.many1 PChar.alphaNum
   position <- PP.getPosition
-  tabsOrWhitespacesOrNewLines
+  _ <- tabsOrWhitespacesOrNewLines
   return (Tag (P.sourceLine position) tagName)
 
+tabsOrWhitespacesOrNewLines :: PB.Parser [Char]
 tabsOrWhitespacesOrNewLines = do
   CA.many (PC.choice [
       (PChar.char '\n'),
